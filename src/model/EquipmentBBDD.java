f58fdcb0468e46e5b55773969309377aa3346f39
Jvargast <jose.vargas.t@csi.cl>
2020-10-24 14:26:12 -0300

    Equipo

---
+++ src/model/EquipmentBBDD.java
@@ -0,0 +1,63 @@
+
+package model;
+
+
+public class EquipmentBBDD {
+    private int eqp_id;
+    private int classroom_id;
+    private String eqp_name;
+    private String status;
+    private String brand;
+    private String year;
+
+    public int getEqp_id() {
+        return eqp_id;
+    }
+
+    public void setEqp_id(int eqp_id) {
+        this.eqp_id = eqp_id;
+    }
+
+    public int getClassroom_id() {
+        return classroom_id;
+    }
+
+    public void setClassroom_id(int classroom_id) {
+        this.classroom_id = classroom_id;
+    }
+
+    public String getEqp_name() {
+        return eqp_name;
+    }
+
+    public void setEqp_name(String eqp_name) {
+        this.eqp_name = eqp_name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+    
+    
+    
+}
