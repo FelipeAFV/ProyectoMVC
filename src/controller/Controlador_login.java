8bc8110c3677d78dc723242bd363c2391db05538
unknown <samuelborraccini@hotmail.com>
2020-10-29 16:54:53 -0300

    controler mod

--- src/controller/Controlador_login.java
+++ src/controller/Controlador_login.java
@@ -12,6 +12,12 @@ import view.Vista_login;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
+import model.AdminBBDD;
+import model.EncargadoDAOImpl;
+import model.TeacherBBDD;
+import view.AdminView;
+import view.TeacherView;
+import view.Vista_encargado;
 
 /**
  *
@@ -35,7 +41,25 @@ public class Controlador_login implements ActionListener {
         user.setUser_name(view.texto_username.getText());
         user.setPassword(String.valueOf(view.texto_password.getPassword()));
         if(aut.authentic(user)){
-            view.setVisible(false);
+            if(user.getJob_title().equals("Teacher")){
+                TeacherView vistateach = new TeacherView();
+                TeacherBBDD teach = new TeacherBBDD();
+                TeacherControl controlteach = new TeacherControl(vistateach, teach);
+                this.view.setVisible(false);
+                controlteach.begin();
+            }else if(user.getJob_title().equals("Admin")){
+                AdminView vistaadm = new AdminView();
+                AdminBBDD admin = new AdminBBDD();
+                AdminControl controladmin = new AdminControl(vistaadm, admin);
+                this.view.setVisible(false);
+                controladmin.begin();
+            }else if(user.getJob_title().equals("Encargado")){
+                Vista_encargado vistaenc = new Vista_encargado();
+                EncargadoDAOImpl enc = new EncargadoDAOImpl();
+                ControladorEncargado controladorenc = new ControladorEncargado(vistaenc,enc);
+                this.view.setVisible(false);
+                controladorenc.begin();
+            }
         }else{
             JOptionPane.showMessageDialog(null, "user not found");
         }
